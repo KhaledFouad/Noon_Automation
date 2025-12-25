6f97fbb2565ceb98cdbb766d07afaa82d780a0de
Khaled Fouad <khaledfouad687@gmail.com>
2025-12-25 23:00:24 +0200

    inserting price and sorting options

--- src/main/java/pages/ElectronicsPage.java
+++ src/main/java/pages/ElectronicsPage.java
@@ -1,9 +1,7 @@
 package pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 
@@ -16,14 +14,19 @@ public class ElectronicsPage  extends  BasePage {
         super(driver);
     }
 
-    public  void goToSamsungBrand() {
+    public  SamsungPage goToSamsungBrand() {
         try {
             wait.until(ExpectedConditions.elementToBeClickable(samsung)).click();
             wait.until(ExpectedConditions.urlContains("/egypt-en/samsung"));
         } catch (Exception e) {
             ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 2000);");
+            new Actions(driver).sendKeys(Keys.ESCAPE).perform();
+            new Actions(driver).moveToElement(driver.findElement(samsung)).perform();
             wait.until(ExpectedConditions.elementToBeClickable(samsung)).click();
             wait.until(ExpectedConditions.urlContains("/egypt-en/samsung"));
+
         }
+        return new SamsungPage(driver);
     }
+
 }
--- src/main/java/pages/SamsungPage.java
+++ src/main/java/pages/SamsungPage.java
@@ -1,12 +1,31 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class SamsungPage extends  BasePage {
+    private final By sortingFilter = By.xpath("//span[text()='Sort By']");
+    private final By bestRatedButton = By.xpath("//a[text()='Best Rated']");
+    private final By priceFilter = By.xpath("//h3[text()='Price']");
+    private final By priceMinButton =  By.name("min");
+    private final By priceMaxButton = By.name("max");
 
     public SamsungPage(WebDriver driver) {
         super(driver);
     }
+  public void sortByBestRated(){
+wait.until( ExpectedConditions.elementToBeClickable(sortingFilter)).click();
+wait.until( ExpectedConditions.elementToBeClickable(bestRatedButton)).click();  ;
 
+  }
 
+    public void filterByPrice(String minPrice , String maxPrice) {
+        wait.until(ExpectedConditions.elementToBeClickable(priceFilter)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(priceMinButton)).clear();
+        driver.findElement(priceMinButton).sendKeys( minPrice);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(priceMaxButton)).clear();
+        driver.findElement(priceMaxButton).sendKeys( maxPrice);
+        driver.findElement(priceMaxButton).submit();
+ }
 }
